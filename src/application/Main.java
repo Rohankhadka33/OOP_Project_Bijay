69d9a168365ecb7455dee6ad415b5c67bfff1740
bijaygautm <bijay.gautam1501@gmail.com>
2021-09-25 22:34:52 +0545

    "FRAMEWORK DONE"

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,4 +1,26 @@
 package application;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(
+                "/view/MainPanel.fxml")));
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.setTitle("Library");
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
