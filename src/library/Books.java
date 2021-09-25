a50f4a92c93b5f078b00d76a5256d5c27904b511
bijaygautm <bijay.gautam1501@gmail.com>
2021-09-25 22:41:31 +0545

    "Class Added Books.java"

--- src/library/Books.java
+++ src/library/Books.java
@@ -1,4 +1,38 @@
 package library;
 
+@SuppressWarnings("all")
 public class Books {
+
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+    private int pages;
+
+    public Books(int Id, String Title, String Author, int Year, int Pages){
+        this.id = Id;
+        this.title=Title;
+        this.author = Author;
+        this.year=Year;
+        this.pages=Pages;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getPages() {
+        return pages;
+    }
 }
