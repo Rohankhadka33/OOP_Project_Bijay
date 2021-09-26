d8d98152709636c107d9ac3e48a1d3cae0b7bde8
bijaygautm <bijay.gautam1501@gmail.com>
2021-09-26 21:48:24 +0545

    "Main Controller Set"

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,4 +1,60 @@
 package controller;
 
-public class MainController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import library.Books;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private TextField tfId;
+    @FXML
+    private TextField tfTitle;
+    @FXML
+    private TextField tfAuthor;
+    @FXML
+    private TextField tfYear;
+    @FXML
+    private TextField tfPages;
+    @FXML
+    private TableView<Books> tvBooks;
+    @FXML
+    private TableColumn<Books, Integer> colId;
+    @FXML
+    private TableColumn<Books, String> colTitle;
+    @FXML
+    private TableColumn<Books, String> colAuthor;
+    @FXML
+    private TableColumn<Books, Integer> colYear;
+    @FXML
+    private TableColumn<Books, Integer> colPages;
+    @FXML
+    private Button btnInsert;
+    @FXML
+    private Button btnUpdate;
+    @FXML
+    private Button btnDelete;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        showBooks();
+    }
+    public void showBooks(){
+
+    }
 }
