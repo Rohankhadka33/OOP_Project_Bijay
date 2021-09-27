98dc79c05c3a6bbf309b4af75b3173aef19a7e26
bijaygautm <bijay.gautam1501@gmail.com>
2021-09-27 21:44:04 +0545

    "Minor Bugs Fixed"

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -73,7 +73,7 @@ public class MainController implements Initializable {
     public Connection getConnection(){
         Connection conn;
         try{
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/yajib", "root","SiGMsDCr7");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/yajibbb", "root","SiGMsDCr7");
             return conn;
         }catch(Exception ex){
             System.out.println("Error: " + ex.getMessage());
